61a80682871414c7f0adf406e75a584ed6473cd9
Dan Banfield <dandbanfield@gmail.com>
2023-03-09 10:24:05 +0000

    Add project files.

---
+++ ChatGPT.NET/ChatGPT.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace ChatGPT.NET
+{
+    public class ChatGPT
+    {
+        #region Private Instance Variables
+
+        private string apiKey = "";
+
+        #endregion
+
+        public ChatGPT(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public async Task<string> GenerateResponseAsync()
+        {
+            await Task.Delay(1000);
+            return "This is just a test.";
+        }
+    }
+}
