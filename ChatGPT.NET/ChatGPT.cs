c753cd4b343040a474527dacdf1d3e50557c984d
Dan Banfield <dandbanfield@gmail.com>
2023-03-09 10:45:56 +0000

    Add generate response functionallity.

--- ChatGPT.NET/ChatGPT.cs
+++ ChatGPT.NET/ChatGPT.cs
@@ -1,4 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
 
 namespace ChatGPT.NET
 {
@@ -10,15 +15,98 @@ namespace ChatGPT.NET
 
         #endregion
 
+        private const string API_ENDPOINT 
+            = "https://api.openai.com/v1/chat/completions";
+
         public ChatGPT(string apiKey)
         {
             this.apiKey = apiKey;
         }
 
-        public async Task<string> GenerateResponseAsync()
+        public async Task<ChatGPTResponse> GenerateResponseAsync(string prompt)
         {
-            await Task.Delay(1000);
-            return "This is just a test.";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization
+                        = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                using (HttpRequestMessage httpRequestMessage 
+                    = new HttpRequestMessage(HttpMethod.Post, API_ENDPOINT))
+                {
+                    string model = "gpt-3.5-turbo";
+                    var messages = new[] { new { role = "user", content = prompt } };
+                    var data = new { model, messages };
+
+                    StringContent content 
+                        = new StringContent(JsonSerializer
+                        .Serialize(data), System.Text.Encoding.UTF8, 
+                        "application/json");
+
+                    httpRequestMessage.Content = content;
+
+                    using (HttpResponseMessage httpResponseMessage 
+                        = await httpClient.SendAsync(httpRequestMessage))
+                    {
+                        if (!httpResponseMessage.IsSuccessStatusCode)
+                            return new ChatGPTResponse("", false);
+
+                        Stream streamResponse 
+                            = await httpResponseMessage
+                            .Content.ReadAsStreamAsync();
+
+                        Root root = await JsonSerializer
+                            .DeserializeAsync<Root>(streamResponse);
+
+                        ChatGPTResponse result = new ChatGPTResponse(root.choices[0]
+                            .message.content.Trim(), true);
+
+                        return result;
+                    }
+                }
+            }
         }
+    }
+
+    public class ChatGPTResponse
+    {
+        public string responseText { get; set; }
+        private bool successfulRequest { get; set; }
+
+        public ChatGPTResponse(string responseText, bool successfulRequest)
+        {
+            this.responseText = responseText;
+            this.successfulRequest = successfulRequest;
+        }
+
+        public bool SuccessfulRequest() => successfulRequest;
+    }
+
+    public class Choice
+    {
+        public int index { get; set; }
+        public Message message { get; set; }
+        public string finish_reason { get; set; }
+    }
+
+    public class Message
+    {
+        public string role { get; set; }
+        public string content { get; set; }
+    }
+
+    public class Root
+    {
+        public string id { get; set; }
+        public string @object { get; set; }
+        public int created { get; set; }
+        public List<Choice> choices { get; set; }
+        public Usage usage { get; set; }
+    }
+
+    public class Usage
+    {
+        public int prompt_tokens { get; set; }
+        public int completion_tokens { get; set; }
+        public int total_tokens { get; set; }
     }
 }
